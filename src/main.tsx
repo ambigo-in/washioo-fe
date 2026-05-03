5f6328ba335cef891f9e99c391b6ef195b75f3cb
avi <avineshwarthegreat@gmail.com>
2026-05-03 19:16:23 +0530

    feat: Add Netlify configuration and routing for static site deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import "./index.css";
 import App from "./App";
 import { store } from "./store/store";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
